ab25a2e85cccd928e1c9b02d9e134d355237342e
just born <justborney@gmail.com>
2023-02-03 16:46:02 +0500

    first attempt

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "meme"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day!",
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "Domestic violence",
+        },
+        {
+            "author": {"username": "Чел"},
+            "body": "Чел...",
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
