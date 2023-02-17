2d338e9a94f15bca3a6c14f9b9f201c311a6a9c7
just born <justborney@gmail.com>
2023-02-17 16:44:35 +0500

    add: Simple Email Framework

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import logging
+
 # import os
 import os.path
+
 # from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 
@@ -9,6 +11,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from config import Config
 
@@ -18,6 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
+mail = Mail(app)
 
 if not app.debug:
     # if app.config["MAIL_SERVER"]:
